6f385e67fa1a166cfd0d88b9f948b7e776db5a90
Alexandr Katyshev <katyshev1@gmail.com>
2023-03-15 19:31:36 +0200

    added routes in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy } from 'react';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<SharedLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/movies" element={<Movies />} />
+                    <Route path="/movies/:movieId" element={<MovieDetails />}>
+                        <Route
+                            path="/movies/:movieId/cast"
+                            element={<Cast />}
+                        />
+                        <Route
+                            path="/movies/:movieId/reviews"
+                            element={<Reviews />}
+                        />
+                    </Route>
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Route>
+            </Routes>
+        </>
+    );
 };
---
+++ src/services/moviesAPI.jsx
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+const API_KEY = 'a2b23be55398d79ed0c58fbe13d82482';
+const BASE_URL = 'https://api.themoviedb.org/3/';
